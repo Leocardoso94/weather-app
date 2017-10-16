985e0b38987948b04e08d0e683abde1fbcaf10de
Leonardo Cardoso <leocardosoti@gmail.com>
2017-10-16 14:22:07 -0200

    searchBar setup

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class SeachBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' }
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        const term = event.target.value;
+
+        this.setState({ term })
+    }
+
     render() {
         return (
-            <div>
-
-            </div>
+            <form className="input-group">
+                <input
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
         );
     }
 }
